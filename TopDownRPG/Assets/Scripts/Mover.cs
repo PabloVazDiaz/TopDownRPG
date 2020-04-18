fecc49e403ff0f1ed4b21252e1afbec86d40293c
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-04-18 11:53:21 +0200

    Basic PointAndClick Navmesh movement

---
+++ TopDownRPG/Assets/Scripts/Mover.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Vector3 target;
+
+    Animator animator;
+    NavMeshAgent navMeshAgent;
+    Ray ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+
+    private void MoveToCursor()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        target = hit.point;
+        navMeshAgent.SetDestination(target);
+    }
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localvelocity = transform.InverseTransformDirection(velocity);
+        float speed = localvelocity.z;
+        animator.SetFloat("forwardSpeed", speed);
+    }
+}
