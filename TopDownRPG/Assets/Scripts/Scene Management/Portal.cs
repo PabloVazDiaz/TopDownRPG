2a2ed7faac1005263ca01bb093d871ae4bd0a144
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-04-22 22:18:06 +0200

    Scene Load System

---
+++ TopDownRPG/Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System;
+
+namespace RPG.SceneManagement
+{
+    
+    public class Portal : MonoBehaviour
+    {
+
+        enum DestinationIdentifier
+        {
+            A, B, C, D
+        }
+
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+
+                return portal;
+            }
+            return null;
+        }
+    }
+
+}
