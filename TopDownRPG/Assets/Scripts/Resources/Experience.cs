78b00b3a81dd4d75ed9f45227af3735751e8e07f
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-04-27 12:38:28 +0200

    Experience Savable

--- TopDownRPG/Assets/Scripts/Resources/Experience.cs
+++ TopDownRPG/Assets/Scripts/Resources/Experience.cs
@@ -1,17 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Resources
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour,ISaveable
     {
 
         [SerializeField] float experiencePoints = 0;
 
+
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
         }
     }
 
