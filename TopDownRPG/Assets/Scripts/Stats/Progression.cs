409997054d42d1ab488dd3a9993d70d409d187b5
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-04-27 13:33:31 +0200

    GetStats Performance Improvement

--- TopDownRPG/Assets/Scripts/Stats/Progression.cs
+++ TopDownRPG/Assets/Scripts/Stats/Progression.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace RPG.Stats
 {
@@ -8,26 +10,37 @@ namespace RPG.Stats
 
         [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
         public float GetStat(Stat stat, CharacterClass characterClass, int level)
         {
+            BuildLookup();
+
+            float[] levels = lookupTable[characterClass][stat];
+            if (levels.Length < level) return 0;
+
+            return levels[level - 1];
+ 
+        }
+
+        private void BuildLookup()
+        {
+            if (lookupTable != null) return;
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
             foreach (ProgressionCharacterClass progressionClass in characterClasses)
             {
-                if (progressionClass.characterClass != characterClass) continue;
+                var statLookupTable = new Dictionary<Stat, float[]>();
 
                 foreach (ProgressionStat progressionStat in progressionClass.stats)
                 {
-                    if (progressionStat.stat != stat) continue;
-
-                    if (progressionStat.levels.Length < level) continue;
-
-                    return progressionStat.levels[level - 1];
-                    
+                    statLookupTable[progressionStat.stat] = progressionStat.levels;
                 }
 
-
+                lookupTable[progressionClass.characterClass] = statLookupTable;
             }
-            
-            return 0f;
+
         }
 
         [System.Serializable]
