a91fd337617234e59014e9d26b89a6d67ea1ef2b
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-04-28 17:08:18 +0200

    NavMesh Disconnect regions and long paths disable

--- TopDownRPG/Assets/Scripts/Control/PlayerController.cs
+++ TopDownRPG/Assets/Scripts/Control/PlayerController.cs
@@ -2,8 +2,8 @@
 using UnityEngine.EventSystems;
 using RPG.Movement;
 using System;
-using RPG.Combat;
 using RPG.Resources;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -21,7 +21,9 @@ namespace RPG.Control
             public Vector2 hotspot;
         }
 
+        [SerializeField] float maxNavMeshProjectionDistance = 1f;
         [SerializeField] CursorMapping[] cursorMappings = null;
+        [SerializeField] float MaxPathLenght = 40f;
 
         private void Awake()
         {
@@ -108,7 +110,11 @@ namespace RPG.Control
         private bool InteractWithMovement()
         {
             RaycastHit hit;
-            if (Physics.Raycast(GetMouseRay(), out hit))
+            Physics.Raycast(GetMouseRay(), out hit);
+            Vector3 target;
+            bool hasHit = RaycastNavMesh(out target);
+
+            if (hasHit)
             {
                 if(Input.GetButton("Fire1"))
                     mover.StartMoveAction(hit.point, 1f);
@@ -116,6 +122,39 @@ namespace RPG.Control
                 return true;
             }
             return false;
+        }
+
+        private bool RaycastNavMesh(out Vector3 target)
+        {
+            target = new Vector3();
+            RaycastHit hit;
+            if (!Physics.Raycast(GetMouseRay(), out hit))
+                return false;
+            NavMeshHit navHit;
+            if (!NavMesh.SamplePosition(hit.point, out navHit, maxNavMeshProjectionDistance, NavMesh.AllAreas))
+                return false;
+            
+            target = navHit.position;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            if (!hasPath) return false;
+            if(path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLenght(path) > MaxPathLenght) return false;
+
+            return true;
+            
+            
+        }
+
+        private float GetPathLenght(NavMeshPath path)
+        {
+            float totalDistance = 0;
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                totalDistance += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+            return totalDistance;
         }
 
         private static Ray GetMouseRay()
